52da9047d0574932bcc69c0772074477f516a1b2
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-11-11 20:36:37 +0500

    Updating

--- Python_with_Os/csv_module.py
+++ Python_with_Os/csv_module.py
@@ -12,6 +12,6 @@ f.close()
 hosts = [['workstation.local', '192.168.1'], ['workstation.local', '192.168.1'], ['workstation.local', '192.168.1']]
 with open('hosts.csv', 'w') as hosts_csv:
     writer = csv.writer(hosts_csv)
-    writer.writerows(hosts)
+    writer.writerows(hosts
     
 
