5e83bbf2ad7e0892d7fa790394946cf034b423b2
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-11-15 13:15:46 +0500

    bash work

---
+++ Python_with_Os/basic_bash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+line="----------------------"
+echo "starting at: $(date)";echo $line
+echo "Uptime:";uptime;echo $line
+echo "Free:";free ;echo $line
+echo "WHO:";who;echo $line
+echo "Finishing at: $(date)" ;echo $line
